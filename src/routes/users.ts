a53f98a632ee98ee18b2da05320c3a2e1af8b1fb
JAMM0118 <jhon.alejandro.correounivalle.edu.co>
2025-11-21 01:43:36 -0500

    funcionalidad editar usuario hecha

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,9 +1,9 @@
-import { Router, Request, Response } from "express";
+import e, { Router, Request, Response } from "express";
 import { User } from "../models/User";
 import bcrypt from "bcrypt";
 import { generateToken, verifyJwt } from "../middlewares/jwt.middleware";
 import { authorizeRoles } from "../middlewares/role.middleware";
-import { registerSchema } from "../validator/user.validator";
+import { registerUserSchema, updateUserSchema } from "../validator/user.validator";
 
 const router = Router();
 
@@ -43,7 +43,7 @@ router.get("/allUsers", verifyJwt, authorizeRoles("admin"), async (req: Request,
  *                email:
  *                  type: string
  *                phoneNumber:
- *                    type: number
+ *                    type: string
  *                workingHours:
  *                    type: string
  *                socialMedia:
@@ -63,7 +63,7 @@ router.get("/allUsers", verifyJwt, authorizeRoles("admin"), async (req: Request,
 router.post("/registerUser", async (req: Request, res: Response) => {
   try {
     
-    const { error } = registerSchema.validate(req.body);  
+    const { error } = registerUserSchema.validate(req.body);  
     if (error) {
       return res.status(400).json({ message: error.details[0].message });
     }
@@ -83,6 +83,81 @@ router.post("/registerUser", async (req: Request, res: Response) => {
 
   } catch (error: any) {
     res.status(500).json({ message: 'Error al procesar la creacion del usuario', error: error.message });
+  }
+});
+
+/**
+ * @swagger
+ * /api/users/updateUser/{id}:
+ *   put:
+ *     summary: Editar Usuario
+ *     tags: [Usuarios]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID del usuario a editar
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *                email:
+ *                  type: string
+ *                phoneNumber:
+ *                    type: string
+ *                workingHours:
+ *                    type: string
+ *                socialMedia:
+ *                    type: string
+ *                address:
+ *                    type: string
+ *                password:
+ *                    type: string
+ *                role:
+ *                    type: string
+ *                fullName:
+ *                    type: string
+ *     responses:
+ *       201:
+ *         description: Usuario editado
+ */
+router.put("/updateUser/:id", async (req: Request, res: Response) => {
+  try {
+    const { error } = updateUserSchema.validate(req.body);  
+    if (error) {
+      return res.status(400).json({ message: error.details[0].message });
+    }
+    const { id } = req.params;
+    const user = await User.findByPk(id);
+    if (!user) {
+      return res.status(404).json({ message: 'Usuario no encontrado' });
+    }
+    const { email} = req.body;
+    if (email) {
+      const sameEmailUser = await User.findOne({ where: { email: email } });
+      if (sameEmailUser) {
+      return res.status(400).json({ message: 'Este correo ya está en uso por otro usuario' });
+      }
+    }
+
+    const {password} = req.body;
+    if(!password){
+      await user.update(req.body);
+      return res.status(201).json({ message: 'Usuario editado exitosamente' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await user.update({...req.body, password: hashedPassword});
+
+    res.status(201).json({ message: 'Usuario editado exitosamente' });
+
+  } catch (error: any) {
+    res.status(500).json({ message: 'Error al procesar la edicion del usuario', error: error.message });
   }
 });
 
--- src/validator/user.validator.ts
+++ src/validator/user.validator.ts
@@ -1,6 +1,6 @@
 import Joi from "joi";
 
-export const registerSchema = Joi.object({
+export const registerUserSchema = Joi.object({
     email: Joi.string().email().required().messages({
         'string.email': 'El correo electrónico no es válido',
         'any.required': 'El correo electrónico es obligatorio',
@@ -21,16 +21,37 @@ export const registerSchema = Joi.object({
         'any.required': 'El nombre completo es obligatorio',
         'string.empty': 'El nombre completo no puede estar vacío',
     }),
-
     role: Joi.string().valid('admin', 'vendedor', 'comprador').required().messages({
         'any.required': 'El rol es obligatorio',
         'string.empty': 'El rol no puede estar vacío',
         'any.only': 'El rol debe ser "admin" , "vendedor" o "comprador"',
     }),
     socialMedia: Joi.string().allow(null),
-
     workingHours: Joi.string().allow(null),
-
     address: Joi.string().allow(null),
 
+});
+
+export const updateUserSchema = Joi.object({
+    email: Joi.string().email().messages({
+        'string.email': 'El correo electrónico no es válido',
+        'string.empty': 'El correo electrónico no puede estar vacío',
+    }),
+    password: Joi.string().min(8).messages({
+        'string.min': 'La contraseña debe tener al menos 8 caracteres',
+        'string.empty': 'La contraseña no puede estar vacía',
+    }),
+    phoneNumber: Joi.string().length(10).pattern(/^\d+$/).messages({
+        'string.pattern.base': 'El número de teléfono solo puede contener números',
+        'string.length': 'El número de teléfono debe contener 10 dígitos',
+        'string.empty': 'El número de teléfono no puede estar vacío',
+    }),
+    fullName: Joi.string().messages({}),
+    role: Joi.string().valid('admin', 'vendedor', 'comprador').messages({
+        'any.only': 'El rol debe ser "admin" , "vendedor" o "comprador"',
+        'string.empty': 'El rol no puede estar vacío',
+    }),
+    socialMedia: Joi.string().allow(null),
+    workingHours: Joi.string().allow(null),
+    address: Joi.string().allow(null),
 });
